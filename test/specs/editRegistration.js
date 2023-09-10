3769451352e2c0e0a6b4f879fe665a45fb056daa
Rajneesh-S <140791190+Rajneesh-S@users.noreply.github.com>
2023-09-10 23:17:04 +0530

    real edit registration information

--- test/specs/editRegistration.js
+++ test/specs/editRegistration.js
@@ -8,9 +8,9 @@ describe(' visit the login page', function(){
 
     it('should able to login',async()=>{
         let userName = await $('[id="username"]');
-        await userName.setValue('raj');
+        await userName.setValue('admin');
         let userPassword = await $('[id="password"]');
-        await userPassword.setValue('Rajneesh987');
+        await userPassword.setValue('Admin123');
         await $('[id="Inpatient Ward"]').click();
         await $('[id="loginButton"]').click();
         // await browser.pause(3000);
@@ -20,78 +20,144 @@ describe(' visit the login page', function(){
     
     
 })
+describe('register a patient',function(){
+    it('register patient',async()=>{
+        await $('#referenceapplication-registrationapp-registerPatient-homepageLink-referenceapplication-registrationapp-registerPatient-homepageLink-extension').click();
+        await $('[name="givenName"]').setValue('axel');
+        await $('[name="familyName"]').setValue('singh');
+        await $('#next-button').click();
+        await $('#gender-field').click();
+        await $('//*[@id="gender-field"]/option[1]').click();
+        await $('//*[@id="next-button"]/icon').click();
+        await $('#birthdateDay-field').setValue('12');
+        await $('#birthdateMonth-field').selectByAttribute('value','2');
+        await $('#birthdateYear-field').setValue('2023');
+        await $('#next-button').click();
+        await $('#address1').setValue('qwer');
+        await $('#confirmation_label').click();
+        await $('//*[@id="submit"]').click();
+        await browser.pause(4000);
+        await $('//*[@id="org.openmrs.module.coreapps.createVisit"]/div/div[2]').click();
+        await $('#start-visit-with-visittype-confirm').click();
+        await $('//*[@id="breadcrumbs"]/li[1]/a/i').click();
+  
+        
+    })
+
+})
 
 describe('edit the registration information', function(){
-    it('should able to goto the manage accounts', async()=>{
-        let systemadministrationButton = await $('//*[@id="coreapps-systemadministration-homepageLink-coreapps-systemadministration-homepageLink-extension"]/i');
-        await systemadministrationButton.click();
-        let manageAccountButton = await $('#org-openmrs-module-adminui-accounts-app');
-        await manageAccountButton.click();
+    it('should able to goto the patient record page', async()=>{
+        let patientRecordButton = await $('//*[@id="coreapps-activeVisitsHomepageLink-coreapps-activeVisitsHomepageLink-extension"]');
+        await patientRecordButton.click();
+         await browser.pause(3000);
 
-        await expect($('//*[@id="content"]/a')).toBeDisplayed();
+        await expect($('//*[@id="content"]/h2')).toBeDisplayed();
+       
     })
 
-    it('should be able to search user',async()=>{
-        let serachButton = await $('//*[@id="list-accounts_filter"]/label/input');
-        await serachButton.setValue('Raj');
-        await expect($('//*[@id="list-accounts"]/tbody/tr/td[1]')).toBeDisplayed();
+    it('should be able to search patient ',async()=>{
+        let serachButton = await $('#patient-search');
+        await serachButton.setValue('axel');
+        await browser.pause(3000);
+        await expect($('//*[@id="patient-search-results-table"]/tbody/tr/td[2]')).toBeDisplayed();
+        
         
 
     })
-    it('should be able to goto the  edit  person details and click on edit button ',async()=>{
-        let editButton = await $('//*[@id="list-accounts"]/tbody/tr/td[5]/i');
-        await editButton.click();
-        let textPersonDetails = await $('//*[@id="adminui-person-details"]/form/div/fieldset/legend');
-        
-        await expect(textPersonDetails).toBeDisplayed();
+
+    it('should be able to goto the  patient details page ',async()=>{
+        await $('//*[@id="patient-search-results-table"]/tbody/tr/td[2]').click();
+
+    
        
 
 
     })
 
-    it('should be able to edit the person given name', async()=>{
+    it('should be able to visit the edit information page', async()=>{
 
-        await $('//*[@id="adminui-person-details"]/form/div/fieldset/div[1]/i').click();
+        await $('//*[@id="application.registrationapp.summary.editPatientLink"]/div/div[2]').click();
 
-        let personNameInputButton = await $('//*[@id="adminui-givenName-field"]');
-        await personNameInputButton.click();
-        await personNameInputButton.setValue('Raja');
-        let saveButton = await $('//*[@id="adminui-person-save"]');
-        await saveButton.click();
-        await browser.pause(5000);
-        let changeName = await $('//*[@id="adminui-person-details"]/form/div/fieldset/div[1]/table/tbody/tr[2]/td').getText();
-        expect(changeName).toEqual('Raja');
+        
+        expect('//*[@id="content"]/div[9]/div[1]/div/div[1]/div[1]/div[1]/h3').toBeDisplayed();
 
     })
 
-    it('should be able to edit the person family name', async()=>{
+    it('should be able to edit the name in demographics ', async()=>{
 
-        await $('//*[@id="adminui-person-details"]/form/div/fieldset/div[1]/i').click();
+       let editDemographicButton =  await $('#demographics-edit-link').click();
+       await browser.pause(3000);
 
-        let familyNameInputButton = await $('#adminui-familyName-field');
-        await familyNameInputButton.click();
-        await familyNameInputButton.setValue('mishra');
-        let saveButton = await $('//*[@id="adminui-person-save"]');
+        let givenNameInput = await $('[name="givenName"]');
+        
+        await givenNameInput.setValue('rohit');
+
+        // let familyName = await $('//*[@id="fr8896-field"]');
+        // await familyName.setValue('shetty');
+
+        let saveButton = await $('#save-form');
         await saveButton.click();
-        await browser.pause(3000);
-        let changefamilyName = await $('//*[@id="adminui-person-details"]/form/div/fieldset/div[1]/table/tbody/tr[1]/td').getText();
-        expect(changefamilyName).toEqual('mishra');
+       
+
+        let confirmButton = await $('#registration-submit');
+        await confirmButton.click();
+        let textName = await $('//*[@id="content"]/div[9]/div[1]/div/div[1]/div[1]/div[2]/div[1]/p').getText();
+
+        await expect(textName).toEqual('rohit');
+
+       
         await browser.pause(4000);
 
     })
 
-    it('should be able to change the username of the user', async()=>{
-        await $('//*[@id="6b464e80-b5be-431d-9cf0-970a8d027df8"]/div[1]/i').click();
+    it('should be able to visit the edit the contact info page', async()=>{
+       let editContactinfo =  await $('#contactInfo-edit-link');
+       await editContactinfo.click();
+       await expect($('//*[@id="content"]/h2')).toBeDisplayed();
         
-        let usernameInput = await $('//*[@id="adminui-username6b464e80-b5be-431d-9cf0-970a8d027df8-field"]');
-        await usernameInput.setValue('rajneesh');
-
-        let saveButton = await $('#adminui-user-save6b464e80-b5be-431d-9cf0-970a8d027df8');
-        await saveButton.click();
+        
 
        
 
     })
+
+    it('should be abale to edit the adress ', async()=>{
+        let address1 = await $('//*[@id="address1"]');
+        await address1.setValue('domlur');
+
+        let saveButton = await $('//*[@id="next-button"]/icon');
+        await saveButton.click();
+
+        await expect($('//*[@id="phoneNumberLabel"]/p/label')).toBeDisplayed();
+    })
+
+    it('should be abale to edit the phone number  less than 10', async()=>{
+        let phoneNumber  = await $('[name="phoneNumber"]');
+        await phoneNumber.setValue('5678');
+
+        let saveButton = await $('#next-button');
+        await saveButton.click();
+
+        await expect($('//*[@id="dataCanvas"]/div/p[2]/span')).toBeDisplayed();
+    })//negative test
+
+    it('should be abale to edit the phone number ', async()=>{
+        let phoneNumber  = await $('//*[@id="formBreadcrumb"]/li[1]/ul/li[2]/span');
+        await phoneNumber.click();
+
+        let editphoneNumber = await $('[name="phoneNumber"]');
+        await editphoneNumber.setValue('9787654532');
+
+        let saveButton = await $('#next-button');
+        await saveButton.click();
+
+        await expect($('//*[@id="dataCanvas"]/div/p[2]/span')).toBeDisplayed();
+
+        let submitButton =await $('#registration-submit');
+        await submitButton.click();
+    })
+
 
 
 })
